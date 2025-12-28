0615d00124fb75edf63bdc916c05c179ef1ee8b8
Nadia Sabrina <sabrinadiaa4@gmail.com>
2025-12-28 16:52:14 +0700

    Refine comments in kontenlms.h for better clarity
    Updated comments for clarity and added details about title search requirements.

--- kontenlms.h
+++ kontenlms.h
@@ -6,14 +6,14 @@
 using namespace std;
 
 struct KontenLMS {
-    string idKonten; // Contoh: MW01, QW01, TW01, PW01, VW01, RW01
-    string judulKonten; // Judul konten (digunakan sebagai key pencarian)
-    string jenisKonten; // Materi, Quiz, PR, Tugas, Video, Referensi
-    int ukuranMB; // Ukuran data dalam satuan MB
+    string idKonten;        // Contoh : MW01, QW01, TW01, PW01, VW01, RW01
+    string judulKonten;     // Judul konten (digunakan sebagai key pencarian, dan pencarian harus sesuai dengan judul konten yang tertera seperti besar kecilnya)
+    string jenisKonten;     // Contoh : Materi, Quiz, PR, Tugas, Video, Referensi
+    int ukuranMB;           // Ukuran data dalam satuan MB untuk keperluan simulasi perhitungan running time
 };
 
 int searchJudulIterative(const vector<KontenLMS>& data, const string& targetJudul);
 
 int searchJudulRecursive(const vector<KontenLMS>& data, const string& targetJudul, int index);
 
-#endif // KONTEN_LMS_H
+#endif
